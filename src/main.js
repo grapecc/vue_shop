638605432c2f7a87eecc69372c2866a206fc7259
Grape <chengrape@foxmail.com>
2020-10-14 21:54:20 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import './assets/css/global.css' 
+
+//配置请求路径的根路径  
+import axios from 'axios'
+///配置根路径 
+axios.defaults.baseURL ='http://127.0.0.1:8888/api/private/v1/'
+
+//axios请求拦截
+axios.interceptors.request.use(config =>{
+  //为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // console.log(config)
+  //在最后必须return config  
+  return config
+});
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,41 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+//引入子页面
+import Welcome  from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
-
+//home 里面配置子路由
 const routes = [
+  {path:'/',redirect:'/login'},
+  {path:'/login',component:Login },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/home',
+    component:Home,
+    redirect:'/welcome',
+    children:[
+      {path:'/welcome',component: Welcome},
+      {path:'/users',component: Users}
+    ]
   }
 ]
 
 const router = new VueRouter({
   routes
 })
+//挂载路由守卫
+router.beforeEach((to,from,next)=>{
+  //to 要访问的路径
+  //from从哪个路径跳来
+  //next是一个函数，表示放行
+  //next()放行 强制跳转next('/login')
 
+  if(to.path ==='/login') return next()
+  //获取token
+   const tokenStr = window.sessionStorage.getItem('token')
+   if(!tokenStr) return next('/login') 
+   next()
+})
 export default router
